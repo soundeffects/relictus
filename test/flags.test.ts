64f19489be893af76215e5a0fc7173437ad92283
soundeffects <james@youngbloods.org>
2021-07-05 21:31:07 -0600

    Testing library back to Jest, started location content

--- test/flags.test.ts
+++ test/flags.test.ts
@@ -1,66 +1,51 @@
-import * as assert from 'assert';
 import * as FlagState from '../src/state/flags';
 
 beforeEach(() => {
   FlagState.resetFlags();
-});
+})
 
-describe('FlagState', () => {
-  describe('activateFlag', () => {
-    
-  });
-
-  describe('flagIsActive', () => {
-    
-  });
-
-  describe('resetFlags', () => {
-    
-  });
-});
-/*
 // activateFlag
 test('activateFlag executes', () => {
   FlagState.activateFlag('new flag');
 });
 
 test('activateFlag does not find flag, returns false', () => {
-  assert.isFalse(FlagState.activateFlag('new flag'));
+  expect(FlagState.activateFlag('new flag')).toBeFalsy();
 });
 
 test('activateFlag finds flag and activates', () => {
-  expect(FlagState.activateFlag('firstView')).toBeTruthy();
+  expect(FlagState.activateFlag('first_view')).toBeTruthy();
 });
 
 test('activateFlag updates score', () => {
   expect(FlagState.score).toEqual(0);
-  FlagState.activateFlag('firstView');
+  FlagState.activateFlag('first_view');
   expect(FlagState.score).toEqual(1);
 })
 
 test('activateFlag does not activate twice', () => {
-  FlagState.activateFlag('firstView');
-  expect(FlagState.activateFlag('firstView')).toBeFalsy();
+  FlagState.activateFlag('first_view');
+  expect(FlagState.activateFlag('first_view')).toBeFalsy();
 });
 
 test('activateFlag finds dependencies, returns false', () => {
-  expect(FlagState.activateFlag('landerConstructed')).toBeFalsy();
+  expect(FlagState.activateFlag('lander_construct')).toBeFalsy();
 });
 
 test('activateFlag finds dependencies, returns true', () => {
-  FlagState.activateFlag('grasperConstructed');
-  expect(FlagState.activateFlag('landerConstructed')).toBeTruthy();
+  FlagState.activateFlag('grasper_attach');
+  expect(FlagState.activateFlag('lander_construct')).toBeTruthy();
 });
 
 test('activateFlag finds multiple dependencies, returns false', () => {
-  FlagState.activateFlag('bayOpened');
+  FlagState.activateFlag('bay_open');
   expect(FlagState.activateFlag('escaped')).toBeFalsy();
 });
 
 
 // flagIsActive
 test('flagIsActive does not find', () => {
-  expect(FlagState.flagIsActive('new flag')).toBeUndefined();
+  expect(FlagState.flagIsActive('new flag')).toBeFalsy();
 });
 
 test('flagIsActive finds, returns false', () => {
@@ -68,23 +53,23 @@ test('flagIsActive finds, returns false', () => {
 });
 
 test('flagIsActive finds, returns true', () => {
-  FlagState.activateFlag('firstView');
-  expect(FlagState.flagIsActive('firstView')).toBeTruthy();
+  FlagState.activateFlag('first_view');
+  expect(FlagState.flagIsActive('first_view')).toBeTruthy();
 });
 
 
 // resetFlags
 test('resetFlags deactivates multiple flags', () => {
-  FlagState.activateFlag('firstView');
-  FlagState.activateFlag('secondBot');
+  FlagState.activateFlag('first_view');
+  FlagState.activateFlag('second_bot');
   
-  expect(FlagState.flagIsActive('firstView')).toBeTruthy();
-  expect(FlagState.flagIsActive('secondBot')).toBeTruthy();
+  expect(FlagState.flagIsActive('first_view')).toBeTruthy();
+  expect(FlagState.flagIsActive('second_bot')).toBeTruthy();
   expect(FlagState.score).toEqual(2);
 
   FlagState.resetFlags();
 
-  expect(FlagState.flagIsActive('firstView')).toBeFalsy();
-  expect(FlagState.flagIsActive('secondBot')).toBeFalsy();
+  expect(FlagState.flagIsActive('first_view')).toBeFalsy();
+  expect(FlagState.flagIsActive('second_bot')).toBeFalsy();
   expect(FlagState.score).toEqual(0);
-});*/
+});
